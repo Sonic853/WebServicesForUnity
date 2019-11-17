50b52acaba6a6163b6cc9a5359169a72ce8dac31
Sonic853 <sonic853@qq.com>
2019-11-17 17:41:58 +0800

    Fixed a Big Wrong. Now it's working my Project.

--- WebService.cs
+++ WebService.cs
@@ -10,34 +10,24 @@
 	用法：
 		String（Get形式：
 			string someone;
-			WebService webService = new WebService();
-        	webService.Get("http://127.0.0.1:853/index.php?user=Sonic853",(downloadHandler)=>{
+			WebService.Instance.Get("http://127.0.0.1:853/index.php?user=Sonic853",(downloadHandler)=>{
 				someone = downloadHandler.text;
 				print(someone);
 			});
-		JSON（Post形式，带上Json数据：
+		JSON（Put形式，带上Json数据：
 			JSONNode jsonData = JSON.Parse("{}");
 			jsonData["Sonic853"].Value = "Is Me!";
 			JSONNode responseJSON;
-			WebService webService = new WebService();
-			webService.Post("http://127.0.0.1:853/index.php",jsonData,(downloadHandler)=>{
+			WebService.Instance.Put("http://127.0.0.1:853/index.php",jsonData,(downloadHandler)=>{
 				responseJSON = JSON.Parse(downloadHandler.text);
 			});
 		Texture（Post形式，带上表单←（应该叫做字典？）：
 			Dictionary<string,string> postDictionary = new Dictionary<string, string>();
 			postDictionary.Add("Lindinia","My Love.");
 			Texture Lindinia;
-			WebService webService = new WebService();
-			webService.Post("https://853lab.com/20190203011324.png",postDictionary,(downloadHandler)=>{
+			WebService.Instance.Post("https://853lab.com/20190203011324.png",postDictionary,(downloadHandler)=>{
 				Lindinia = ((DownloadHandlerTexture)downloadHandler).texture;
 			});
-
-	不知道会不会出问题的用法：
-		string someone;
-		WebService.Instance.Get("http://127.0.0.1:853/index.php",(downloadHandler)=>{
-			someone = downloadHandler.text;
-			print(someone);
-		});
 
 	能力有限，只会弄成这样。。。或者帮忙完善orz
 	（感觉自己封装能力像坨屎一样，但能用
@@ -54,9 +44,10 @@ using UnityEngine.Networking;
 public class WebService : MonoBehaviour
 {
 	// general web service
-	public string Endpoint;
-	public Dictionary<string, string> postVariables;
-	public JSONNode responseJSON;
+	public string URL { get; set; }
+	public Dictionary<string, string> postDictionary { get; set; }
+	public JSONNode jsonData { get; set; }
+	public DownloadHandler downloadHandler { get; set; }
 	
 	// Set up states
     enum State {Idle,Getting, Posting,Responded};
@@ -82,14 +73,39 @@ public class WebService : MonoBehaviour
             return instance;
         }
     }
+    void Awake(){
+        instance = this;
+    }
 
 	public void Start() {
 
 		state = State.Idle;
 
 	}
+	public void Set(string URL){
+		this.URL = URL;
+	}
+	public void Set(string URL, JSONNode jsonData){
+		this.URL = URL;
+		this.jsonData = jsonData;
+	}
+	public void Set(string URL, Dictionary<string, string> postDictionary){
+		this.URL = URL;
+		this.postDictionary = postDictionary;
+	}
 
 	// GET from the server
+    public void Get(){
+
+	    UnityWebRequest response = UnityWebRequest.Get(URL);
+
+		state = State.Getting;
+
+	    StartCoroutine( 
+			MakeRequest(response,null)
+		);
+
+    }
     public void Get(string URL){
 
 	    UnityWebRequest response = UnityWebRequest.Get(URL);
@@ -112,17 +128,33 @@ public class WebService : MonoBehaviour
 		);
 
     }
+    public void Get(CallBack callBack){
 
-	// POST to the server
+	    UnityWebRequest response = UnityWebRequest.Get(URL);
+
+		state = State.Getting;
+
+	    StartCoroutine( 
+			MakeRequest(response,callBack)
+		);
+
+    }
+
+	// POST to the server use Dictionary
     public void Post(string URL, Dictionary<string,string> postDictionary, CallBack callBack){
 
-        WWWForm form = new WWWForm();
+		UnityWebRequest response = UnityWebRequest.Post(URL, postDictionary);
 
-	    foreach(KeyValuePair<String,String> post_arg in postDictionary) {
+		state = State.Posting;
 
-	   		form.AddField(post_arg.Key, post_arg.Value);
+	    StartCoroutine( 
+			MakeRequest(response,callBack)
+		);
 
-	    }
+    }
+
+	// POST to the server use WWWForm
+    public void Post(string URL, WWWForm form, CallBack callBack){
 
 		UnityWebRequest response = UnityWebRequest.Post(URL, form);
 
@@ -134,8 +166,90 @@ public class WebService : MonoBehaviour
 
     }
 
-	// POST to the server use json
-    public void Post(string URL, JSONNode jsonData, CallBack callBack){
+	// Just POST to the server
+    public void Post(string URL, CallBack callBack){
+
+        WWWForm form = new WWWForm();
+
+		UnityWebRequest response = UnityWebRequest.Post(URL, form);
+
+		state = State.Posting;
+
+	    StartCoroutine( 
+			MakeRequest(response,callBack)
+		);
+
+    }
+
+    public void Post(string URL){
+
+        WWWForm form = new WWWForm();
+
+		UnityWebRequest response = UnityWebRequest.Post(URL, form);
+
+		state = State.Posting;
+
+	    StartCoroutine( 
+			MakeRequest(response,null)
+		);
+
+    }
+
+    public void Post(CallBack callBack){
+
+		UnityWebRequest response = UnityWebRequest.Post(URL, postDictionary);
+
+		state = State.Posting;
+
+	    StartCoroutine( 
+			MakeRequest(response,callBack)
+		);
+
+    }
+
+	// PUT to the server use json
+    public void Put(string URL, JSONNode jsonData, CallBack callBack){
+
+        string jsonString = jsonData.ToString() ?? "";
+
+		UnityWebRequest response = UnityWebRequest.Put(URL, jsonString);
+
+		state = State.Posting;
+
+	    StartCoroutine( 
+			MakeRequest(response,callBack)
+		);
+
+    }
+
+    public void Put(string URL, string jsonData, CallBack callBack){
+
+        string jsonString = jsonData;
+
+		UnityWebRequest response = UnityWebRequest.Put(URL, jsonString);
+
+		state = State.Posting;
+
+	    StartCoroutine( 
+			MakeRequest(response,callBack)
+		);
+
+    }
+
+	// PUT to the server use byte
+    public void Put(string URL, byte[] bodyData, CallBack callBack){
+
+		UnityWebRequest response = UnityWebRequest.Put(URL, bodyData);
+
+		state = State.Posting;
+
+	    StartCoroutine( 
+			MakeRequest(response,callBack)
+		);
+
+    }
+
+    public void Put(CallBack callBack){
 
         string jsonString = jsonData.ToString() ?? "";
 
@@ -158,9 +272,11 @@ public class WebService : MonoBehaviour
 
 		Debug.Log("Web Service Response: "+response.downloadHandler.text);
 
+		downloadHandler = response.downloadHandler;
+
 		if(callBack != null) {
 
-			callBack( response.downloadHandler );
+			callBack( downloadHandler );
 
 		}
 
